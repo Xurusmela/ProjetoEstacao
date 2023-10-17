803371bc1e41ffb042f9b589b3c3dbdda63573d9
BiaMayumi <109619902+BiaMayumi@users.noreply.github.com>
2023-10-17 15:31:15 -0300

    Estação do ano

---
+++ season.py
@@ -0,0 +1,15 @@
+mes = input('Diga em que mês você esta: ')
+dia = input('Diga em que dia você esta: ')
+if mes == ('1', '2' or '3') and dia >= '20':
+    print('É verão')
+else:
+    if mes == ('4', '5' or '6') and dia >= '21':
+        print('É outono')
+    else:
+        if mes == '7' or '8' or '9' and dia >= '23':
+            print('É inverno')
+        else:
+            if mes == '10' or '11' or '12' and dia >= '22':
+                print('É primavera')
+            else:
+                print('Esta data não existe')
